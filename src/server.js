7a0f33891a0fd19e3083646691bcedabe9e75d76
Karasik953 <itprogergoit@gmail.com>
2026-01-18 01:12:08 +0300

    fix routes

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import { connectMongoDB } from './db/connectMongoDB.js';
 import { notFoundHandler } from "./middleware/notFoundHandler.js";
 import { errorHandler } from "./middleware/errorHandler.js";
 import { logger } from "./middleware/logger.js";
-import noteRoutes from './routes/noteRoutes.js';
+import noteRoutes from './routes/notesRoutes.js';
 
 
 const app = express();
